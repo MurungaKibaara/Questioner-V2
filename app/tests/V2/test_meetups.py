45ad1aab81125c3a3d0dc23957ae21f2284f73ca
MurungaKibaara <murungakibaara@outlook.com>
2019-01-21 10:33:31 +0300

    [feature #163308809] Added some more tests

---
+++ app/tests/V2/test_meetups.py
@@ -0,0 +1,32 @@
+'''Test Meetups'''
+from .basetests import BaseTest
+
+class TestMeetups(BaseTest):
+    '''Test all meetups'''
+
+    def test_create_meetup(self):
+        '''Test create meetup endpoint'''
+
+        self.registration()
+        response = self.create_meetup()
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_all_meetups(self):
+        '''Test get all meetups endpoint'''
+
+        self.registration()
+        self.create_meetup()
+        response = self.get_all_meetups()
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_meetup(self):
+        '''Test get specific meetup endpoint'''
+
+        response = self.get_all_meetups()
+        self.assertEqual(response.status_code, 200)
+
+    def test_meetup_does_not_exist(self):
+        '''Test get nonexistent meetup'''
+
+        response = self.get_meetup_doesnt_exist()
+        self.assertEqual(response.status_code, 404)
---
+++ app/tests/V2/test_users.py
@@ -0,0 +1,17 @@
+'''Tests for users'''
+from .basetests import BaseTest
+
+class TestUsers(BaseTest):
+    """Test user registration and login"""
+
+    def test_user_registration(self):
+        '''Test register new account'''
+        response = self.registration()
+        self.assertEqual(response.status_code, 201)
+
+
+
+    def test_user_login(self):
+        """Test user login endpoint"""
+        response = self.login()
+        self.assertEqual(response.status_code, 200)
