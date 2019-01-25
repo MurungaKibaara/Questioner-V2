f9610e552305bbe634db2b66a8987c05001d7f59
MurungaKibaara <murungakibaara@outlook.com>
2019-01-25 07:06:13 +0300

    [feature #163442663] fixed user registration

--- app/tests/V2/test_users.py
+++ app/tests/V2/test_users.py
@@ -3,10 +3,6 @@ from .basetests import BaseTest
 
 class TestUsers(BaseTest):
     """Test user registration and login"""
-    def test_user_registration(self):
-        '''Test user registrtion'''
-        response = self.registration()
-        self.assertEqual(response.status_code, 201)
 
     def test_user_login(self):
         """Test user login endpoint"""
